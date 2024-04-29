97b76caf40c4f0bb62fbbb9f4065612d1f9adf58
Dhruv Soni <dhruvsoni@Dhruvs-Laptop.local>
2024-04-30 02:36:57 +0530

    feat: changes made to the console

--- packages/dhruv-2/main.js
+++ packages/dhruv-2/main.js
@@ -3,7 +3,7 @@ import greeting from "dhruv-1";
 function callgreeting()
 {
     console.log(greeting);
-    console.log('New thing added');
+    console.log('New thing added 2');
 }
 
 export default callgreeting;
