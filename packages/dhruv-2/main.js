a4246acd6f8f4e77ffe656b08511ae01407e452e
Dhruv Soni <dhruvsoni@Dhruvs-Laptop.local>
2024-04-30 04:28:14 +0530

    added new feature to dhruv-2

--- packages/dhruv-2/main.js
+++ packages/dhruv-2/main.js
@@ -3,7 +3,7 @@ import greeting from "dhruv-1";
 function callgreeting()
 {
     console.log(greeting);
-    console.log('New thing added 9');
+    console.log('New thing added 10');
 }
 
 export default callgreeting;
